5cb33f6b23a92aabc6888ba6d1f7b881c095f467
s211ergei1907 <Pozdeev.2002@inbox.ru>
2022-11-17 00:28:04 +0300

    dependencies, constraints

--- server/index.js
+++ server/index.js
@@ -1,22 +1,23 @@
-require("dotenv").config();
-const express = require("express");
-const sequelize = require("./db");
+require('dotenv').config()
+const express = require('express')
+const sequelize = require('./db')
+const models = require('./models/models')
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 
-const app = express();
+const app = express()
 
 //Функция для подключения к БД
 //Функция делаем асинхронной, потому что все операции с БД являются асинхронными
 const start = async () => {
-  try {
-    await sequelize.authenticate()  //authenticate() - с помощью нее устанавливается подключение к БД
-    await sequelize.sync()          //sync() - сверяет состояние БД, которое мы опишем
-    app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-  } catch (error) {
-        console.log(error);
-  }
-};
+    try {
+        await sequelize.authenticate()
+        await sequelize.sync()
+        app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+    } catch (e) {
+        console.log(e)
+    } 
+}
 
 
-start();
+start()
