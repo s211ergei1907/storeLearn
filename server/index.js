48794353b8963750a71dab98be49397db4981337
s211ergei1907 <Pozdeev.2002@inbox.ru>
2022-11-18 23:08:53 +0300

    typeCOntroller

--- server/index.js
+++ server/index.js
@@ -2,10 +2,16 @@ require('dotenv').config()
 const express = require('express')
 const sequelize = require('./db')
 const models = require('./models/models')
-
+const cors = require('cors')
 const PORT = process.env.PORT || 5000
 
 const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.status(200).json({message: 'WORNING!!!'})
+})
 
 //Функция для подключения к БД
 //Функция делаем асинхронной, потому что все операции с БД являются асинхронными
