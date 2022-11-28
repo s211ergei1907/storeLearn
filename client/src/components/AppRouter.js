a8abb6c00e4e046503a0c3b7617e75d5dbe2a7c5
s211ergei1907 <Pozdeev.2002@inbox.ru>
2022-11-29 00:24:26 +0300

    routes doesnt work

--- client/src/components/AppRouter.js
+++ client/src/components/AppRouter.js
@@ -1,13 +1,25 @@
 import React from 'react'
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link,
-  } from 'react-router-dom';
-  
+import {Routes, Route, Navigate} from 'react-router-dom'
+import { authRoutes, publicRoutes } from '../routes'; 
+import { SHOP_ROUTE } from '../utils/consts';
+
 export const AppRouter = () => {
+  const isAuth = false
   return (
-    <div>AppRouter</div>
+    //Просто флаг, будем чекать авторизован пользователь или нет
+    //Switch (мы указываем несколько марщрутов(например регистрация, авторизация, и магазин) и если не один не отработает(пользователь ввел некоректный адрес))
+    // тогда отработает самый последний, который указан в этом Routes
+    <Routes>
+      
+      {isAuth && authRoutes.map(({path, Component}) =>
+        <Route key={path} path={path} element={<Component/>} exact/>  //указываем компонент, который по этому пути будет отрисовывать
+      )}
+      {publicRoutes.map(({path, Component}) =>
+      <Route key={path} path={path} element={<Component/>} exact/>
+      )}
+      
+      <Navigate to={SHOP_ROUTE}/>
+    </Routes>
+    
   )
 }
