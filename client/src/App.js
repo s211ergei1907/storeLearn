f2af2b1b2f12446160020745af4b9b1a75606128
s211ergei1907 <Pozdeev.2002@inbox.ru>
2022-12-03 00:32:51 +0300

    page DevicePage

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import { AppRouter } from "./components/AppRouter";
-
+import React, {useContext, useEffect, useState} from 'react';
+import {BrowserRouter} from "react-router-dom";
+import AppRouter from "./components/AppRouter";
+import NavBar from "./components/NavBar";
+import {observer} from "mobx-react-lite";
+import {Context} from "./index";
+import {check} from "./http/userAPI";
+import {Spinner} from "react-bootstrap";
 function App() {
   return (
     (
